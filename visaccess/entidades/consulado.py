bc632c26b6c70a905a0dd828e6dbf929305517f2
Julia <ajulia.fernanda80@gmail.com>
2023-04-10 20:53:28 -0300

    defining project structure

---
+++ visaccess/entidades/consulado.py
@@ -0,0 +1,18 @@
+from entidades.gerente import Gerente
+from entidades.agente import Agente
+
+
+class Consulado:
+
+    def __init__(self, nome):
+        self.__nome = nome
+        self.__gerente = None
+        self.__agentes = list
+
+    def cadastrar_gerente_para_consulado(self, gerente: Gerente):
+        if gerente is not None and isinstance(gerente, Gerente):
+            self.__gerente = gerente
+
+    def cadastrar_agentes_para_consulado(self, agente: Agente):
+        if agente is not None and isinstance(agente, Agente):
+            self.__agentes.append(agente)
