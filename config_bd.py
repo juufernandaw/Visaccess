0f0d3b9358507dd680423915d078ecc582c399a0
Diego Goiana <diegovsg96@gmail.com>
2023-04-12 23:47:45 -0300

    sqlite3

--- config_bd.py
+++ config_bd.py
@@ -1,19 +1,26 @@
-import psycopg2
+import sqlite3
 
-conn = psycopg2.connect(
-    host="192.168.0.35",
-    database="visaccess",
-    user="postgres",
-)
+# Conectar-se ao banco de dados ou criá-lo se ele ainda não existe
+conn = sqlite3.connect('biblioteca.db')
 
-# Cria um cursor para executar comandos SQL
-cur = conn.cursor()
+# Criar a tabela de livros
+conn.execute('''CREATE TABLE livros
+                (id INT PRIMARY KEY NOT NULL,
+                titulo TEXT NOT NULL,
+                autor TEXT NOT NULL,
+                ano_publicacao INT NOT NULL);''')
 
-# Executa um comando SQL
-cur.execute("SELECT * FROM consulado")
+# Inserir dados na tabela
+conn.execute("INSERT INTO livros (id, titulo, autor, ano_publicacao) \
+                VALUES (1, 'Python para Leigos', 'Guido van Rossum', 2020)")
 
-# Obtém os resultados
-results = cur.fetchall()
+conn.execute("INSERT INTO livros (id, titulo, autor, ano_publicacao) \
+                VALUES (2, 'Introdução à Ciência de Dados', 'João da Silva', 2021)")
 
-cur.close()
+# Consultar dados da tabela
+cursor = conn.execute("SELECT id, titulo, autor, ano_publicacao FROM livros")
+for row in cursor:
+    print(f"ID = {row[0]}, Título = {row[1]}, Autor = {row[2]}, Ano de Publicação = {row[3]}")
+
+# Fechar a conexão
 conn.close()
