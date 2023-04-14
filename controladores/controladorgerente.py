56e8487ed65e038d0f2160a33a50826a17c9f59f
Guilherme Focassio dos Santos <guifocassio1@gmail.com>
2023-04-14 19:46:16 -0300

    Atualização no controlador gerente e adição da tela agente
    Para iniciar o cadastro de Agentes, é necessário da tela inicial dos gerentes.

--- controladores/controladorgerente.py
+++ controladores/controladorgerente.py
@@ -1,8 +1,52 @@
-
+from telas.telagerente import TelaGerente
 
 class ControladorGerente:
     def __init__(self):
-        pass
+        self.__tela_gerente = TelaGerente()
 
-    # def verificar_login_senha(self, login, senha):  # VERIFICAR o login e senha.
+    @property
+    def tela_sistema(self):
+        return self.__tela_gerente
 
+    def iniciar_tela_gerente(self):
+        while True:
+            opcao = self.__tela_gerente.tela_gerente_inicial()
+            if opcao == 1:
+                self.__tela_gerente.mostra_mensagem('Cadastrar Solicitação de Visto')
+            elif opcao == 2:
+                self.__tela_gerente.mostra_mensagem('Aprovar Solicitação de Visto')
+            elif opcao == 3:
+                self.abrir_tela_cadastro()
+            elif opcao == 4:
+                self.__tela_gerente.mostra_mensagem('Cadastrar Estrangeiros')
+            elif opcao == 5:
+                self.__tela_gerente.mostra_mensagem('Emitir Relatório de Solicitações Aprovadas')
+            elif opcao == 0:
+                # AQUI TEM QUE VOLTAR PARA A 1 TELA!
+                exit()
+
+    def abrir_tela_cadastro(self):
+        while True:
+            opcao = self.__tela_gerente.tela_cadastro_agentes()
+            if opcao == 1:
+                self.adicionar_agente()
+            elif opcao == 2:
+                self.__tela_gerente.mostra_mensagem('Excluir Agente')
+            elif opcao == 3:
+                self.__tela_gerente.mostra_mensagem('Listar Agentes')
+            elif opcao == 4:
+                self.__tela_gerente.mostra_mensagem('Modificar dados de Agente')
+            elif opcao == 0:
+                # AQUI TEM QUE VOLTAR PARA A TELA INICIAL DO GERENTE!
+                self.iniciar_tela_gerente()
+
+    def adicionar_agente(self):
+        opcao, name = self.__tela_gerente.tela_adicionar_agentes()
+        if opcao == 1:
+            print(name)
+            self.abrir_tela_cadastro()
+            # *pega os dados, testa e joga no BD.
+        elif opcao == 2:
+            self.__tela_gerente.close()
+            self.abrir_tela_cadastro()
+            # só volta e da uma mensagem de erro.
---
+++ telas/telagerente.py
@@ -0,0 +1,101 @@
+import PySimpleGUI as interface_sistema
+
+class TelaGerente:
+    def __init__(self):
+        self.__window = None
+
+    def close(self):
+        self.__window.Close()
+
+    def mostra_mensagem(self, msg):
+        interface_sistema.popup(msg)
+
+    def mensagem(self, texto):
+        return print(texto)
+
+    # telas inicias do gerente:
+    def componentes_tela_gerente_inicial(self):
+        layout = [
+            [interface_sistema.Text('Selecione o que deseja fazer', font=("Helvica", 25))],
+            [interface_sistema.Radio('Cadastrar Solicitação de Visto', "componentes_tela_gerente_inicial", key='1')],
+            [interface_sistema.Radio('Aprovar Solicitação de Visto', "componentes_tela_gerente_inicial", key='2')],
+            [interface_sistema.Radio('Cadastrar Agentes', "componentes_tela_gerente_inicial", key='3')],
+            [interface_sistema.Radio('Cadastrar Estrangeiros', "componentes_tela_gerente_inicial", key='4')],
+            [interface_sistema.Radio('Emitir Relatório de Solicitações Aprovadas', "componentes_tela_gerente_inicial", key='5')],
+            [interface_sistema.Button('Confirmar'), interface_sistema.Button('Voltar')]
+        ]
+        self.__window = interface_sistema.Window('Tela Inicial do Gerente').Layout(layout)
+
+    def tela_gerente_inicial(self):
+        self.componentes_tela_gerente_inicial()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        if values['5']:
+            opcao = 5
+        if button in (None, 'Voltar'):
+            opcao = 0
+        
+        self.close()
+
+        return opcao
+    
+    # tela inicial do cadastro de agente:
+    def componentes_tela_cadastro_agentes(self):
+        layout = [
+            [interface_sistema.Text('Cadastros de Agentes', font=("Helvica", 25))],
+            [interface_sistema.Radio('Adicionar Agente', "componentes_tela_cadastro_agentes", key='1')],
+            [interface_sistema.Radio('Excluir Agente', "componentes_tela_cadastro_agentes", key='2')],
+            [interface_sistema.Radio('Listar Agentes', "componentes_tela_cadastro_agentes", key='3')],
+            [interface_sistema.Radio('Modificar dados de Agente', "componentes_tela_cadastro_agentes", key='4')],
+            [interface_sistema.Button('Confirmar'), interface_sistema.Button('Voltar')]
+        ]
+        self.__window = interface_sistema.Window('Tela de Cadastro de Agente').Layout(layout)
+
+    def tela_cadastro_agentes(self):
+        self.componentes_tela_cadastro_agentes()
+        button, values = self.__window.Read()
+        opcao = 0
+        if values['1']:
+            opcao = 1
+        if values['2']:
+            opcao = 2
+        if values['3']:
+            opcao = 3
+        if values['4']:
+            opcao = 4
+        if button in (None, 'Voltar'):
+            opcao = 0
+        
+        self.close()
+
+        return opcao
+
+    # tela para adicionar agente:
+    def componentes_tela_adicionar_agentes(self):
+        layout = [
+            [interface_sistema.Text('Nome'), interface_sistema.InputText('')],
+            [interface_sistema.Text('CPF'), interface_sistema.InputText('')],
+            [interface_sistema.Text('Senha'), interface_sistema.InputText('')],
+            [interface_sistema.Text('Consulado'), interface_sistema.InputText('')],
+            [interface_sistema.Button('Confirmar'), interface_sistema.Button('Voltar')]
+        ]
+        self.__window = interface_sistema.Window('Adicionar Agente').Layout(layout)
+
+    def tela_adicionar_agentes(self):
+        self.componentes_tela_adicionar_agentes()
+        button, name = self.__window.Read()
+
+        opcao = 1
+        if button in (None, 'Voltar'):
+            opcao = 0
+        
+        self.close()
+        return opcao, name
