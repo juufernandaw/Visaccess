8533861684113343f49e80b8f4aeec6c0496c9a6
Julia <ajulia.fernanda80@gmail.com>
2023-04-16 00:28:38 -0300

    feat: creating view consulado

--- controladores/controlador_consulado.py
+++ controladores/controlador_consulado.py
@@ -1,21 +1,21 @@
-from entidades.consulado import Consulado
-from persistencia.consuladoDAO import ConsuladoDAO
-
+from Visaccess.persistencia.consuladoDAO import ConsuladoDAO
+from Visaccess.entidades.consulado import Consulado
+from Visaccess.telas.tela_consulado import TelaConsulado
 
 class ControladorConsulado:
 
-    def __init__(self, consulado_dao: ConsuladoDAO):
-        self.consulado_DAO = []
+    def __init__(self):
+        self.__consulado_DAO = ConsuladoDAO()
+        self.__consulado_tela = TelaConsulado()
 
-    def incluir_consulado(self, consulado: Consulado):
+    def incluir_consulado(self):
         pass
 
-    def alterar_consulado(self, consulado: Consulado):
+    def alterar_consulado(self):
         pass
 
-    def excluir_consulado(self, consulado: Consulado):
+    def excluir_consulado(self):
         pass
 
     def listar_consulados(self):
         pass
- 
