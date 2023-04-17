7b39a9dacb3cb1ae908a69bb20349d6b9068a823
Diego Goiana <diegovsg96@gmail.com>
2023-04-17 17:36:38 -0300

    login_ir_update_gerente

--- controladores/controladorgerente.py
+++ controladores/controladorgerente.py
@@ -238,23 +238,22 @@ class ControladorGerente:
                 return False
 
 
-    def abre_tela_inicial_gerente(self):  # abre a tela gerente pos login
-        try:
-            usuario = self.__controlador_sistema.usuario_logado
-            mexer_gerente_opcoes = {1: self.voltar_tela_sistema(),
-                                  2: self.voltar_tela_sistema(),
-                                  0: self.voltar_tela_sistema
-                                  }
-            while True:
-                opcao_escolhida = self.__tela_gerente.tela_gerente_inicial()
-                if opcao_escolhida != 1 and opcao_escolhida != 2 and opcao_escolhida != 3 and opcao_escolhida != 4 and opcao_escolhida != 5 and opcao_escolhida != 0:
-                    raise ValueErrorException
-                else:
-                    funcao_escolhida = mexer_gerente_opcoes[opcao_escolhida]
-                    return funcao_escolhida()
-        except ValueErrorException as e:
-            self.__tela_aluno.mostrar_msg(e)
-            self.abre_tela_inicial()
+    # def abre_tela_inicial_gerente(self):  # abre a tela gerente pos login
+    #     try:
+    #         mexer_gerente_opcoes = {1: self.voltar_tela_sistema(),
+    #                               2: self.voltar_tela_sistema(),
+    #                               0: self.voltar_tela_sistema
+    #                               }
+    #         while True:
+    #             opcao_escolhida = self.__tela_gerente.tela_gerente_inicial()
+    #             if opcao_escolhida != 1 and opcao_escolhida != 2 and opcao_escolhida != 3 and opcao_escolhida != 4 and opcao_escolhida != 5 and opcao_escolhida != 0:
+    #                 raise ValueErrorException
+    #             else:
+    #                 funcao_escolhida = mexer_gerente_opcoes[opcao_escolhida]
+    #                 return funcao_escolhida()
+    #     except ValueErrorException as e:
+    #         self.__tela_aluno.mostrar_msg(e)
+    #         self.abre_tela_inicial()
 
     def voltar_tela_sistema(self):
         return self.__controlador_sistema.iniciar_tela_sistema()
